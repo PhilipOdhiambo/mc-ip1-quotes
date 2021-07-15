02f667aeb61771d218d5c504897ac91cc41e36d9
Philip Owino <jofonowino@gmail.com>
2021-07-15 08:45:26 +0300

    create custom attribute directive to higlight top quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { TopQuoteDirective } from './top-quote.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    TopQuoteDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/top-quote.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appTopQuote]'
+})
+export class TopQuoteDirective {
+
+  constructor(private elem:ElementRef) {
+    this.elem.nativeElement.style.backgroundColor = "green";
+  }
+
+}
