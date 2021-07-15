dcec4eeae537e1c9d587ca4ca5e861dd08c8be2f
Philip Owino <jofonowino@gmail.com>
2021-07-15 09:01:01 +0300

    modify topQuote directive to make it visible and to test it

--- src/app/top-quote.directive.ts
+++ src/app/top-quote.directive.ts
@@ -7,6 +7,8 @@ export class TopQuoteDirective {
 
   constructor(private elem:ElementRef) {
     this.elem.nativeElement.style.backgroundColor = "green";
+    this.elem.nativeElement.style.color = "white";
+
   }
 
 }
