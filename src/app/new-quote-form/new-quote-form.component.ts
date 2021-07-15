42ae73226634d75c6f87eb252453519975f0bda9
Philip Owino <jofonowino@gmail.com>
2021-07-15 19:18:11 +0300

    create quote inputs and connect to quote.component

--- src/app/new-quote-form/new-quote-form.component.ts
+++ src/app/new-quote-form/new-quote-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-new-quote-form',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewQuoteFormComponent implements OnInit {
 
-  test:string = '';
+  newQuote:Quote;
 
-  constructor() { }
+  constructor() {
+    this.newQuote = new Quote('','','',new Date())
+  }
+
+  onSubmit(form:any) {
+    
+  }
 
   ngOnInit(): void {
   }
